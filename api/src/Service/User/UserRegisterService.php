b0bcfb8ac9264165c65e0af284fb042e1b3f1bc7
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-07 19:35:08 +0200

    Prepare UserRegisterService & Register controller

---
+++ api/src/Service/User/UserRegisterService.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\User;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserRegisterService
+{
+    public function __construct()
+    {
+    }
+
+    public function create(Request $request): User
+    {
+
+    }
+
+
+}
