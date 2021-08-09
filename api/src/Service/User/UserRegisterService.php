31a39378c639e1a6849bd77e6b8470e667b30a62
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-09 11:16:13 +0200

    Feat: Register users

--- api/src/Service/User/UserRegisterService.php
+++ api/src/Service/User/UserRegisterService.php
@@ -5,17 +5,42 @@ declare(strict_types=1);
 namespace App\Service\User;
 
 use App\Entity\User;
+use App\Exception\User\UserAlreadyExistException;
+use App\Repository\UserRepository;
+use App\Service\Password\EncoderService;
+use App\Service\Request\RequestService;
 use Symfony\Component\HttpFoundation\Request;
 
 class UserRegisterService
 {
-    public function __construct()
+    private UserRepository $userRepository;
+    private EncoderService $encoderService;
+
+    public function __construct(UserRepository $userRepository, EncoderService $encoderService)
     {
+        $this->userRepository = $userRepository;
+        $this->encoderService = $encoderService;
     }
 
     public function create(Request $request): User
     {
+        // Get data from request
+        $name = RequestService::getField($request, 'name');
+        $email = RequestService::getField($request, 'email');
+        $password = RequestService::getField($request, 'password');
 
+        // create new user
+        $user = new User($name, $email);
+        // encode password with encoder service
+        $user->setPassword($this->encoderService->generateEncodedPassword($user, $password));
+
+        try {
+            $this->userRepository->save($user);
+        }catch (\Exception $exception){
+            throw UserAlreadyExistException::fromEmail($email);
+        }
+
+        return $user;
     }
 
 
