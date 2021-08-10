6d1403be6e50c591e92a3f7a8e0374b713c02518
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-10 14:54:43 +0200

    Crear servicio, clase para rutas y

---
+++ mailer/src/Service/Mailer/MailerService.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mailer\Service\Mailer;
+
+class MailerService
+{
+
+    public function __construct()
+    {
+    }
+
+    public function send(): void
+    {
+    }
+
+}
