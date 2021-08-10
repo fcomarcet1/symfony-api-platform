1d7804fb367f8e6bd70496e0c84a83fd4f3c6561
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-10 10:08:16 +0200

    Change namespace App->Mailer && add dependencies

--- mailer/src/Kernel.php
+++ mailer/src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Mailer;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
