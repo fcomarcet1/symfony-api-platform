15cc8a376b2797a07fec80371eaa55bf36da4d88
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-12 09:31:17 +0200

    Add: mailer service

--- mailer/src/Service/Mailer/MailerService.php
+++ mailer/src/Service/Mailer/MailerService.php
@@ -4,15 +4,55 @@ declare(strict_types=1);
 
 namespace Mailer\Service\Mailer;
 
+use Mailer\Templating\TwigTemplate;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
+
+
 class MailerService
 {
 
-    public function __construct()
-    {
+    private const TEMPLATE_SUBJECT_MAP = [
+        TwigTemplate::USER_REGISTER => 'Bienvenid@ a My Finance App!!. '
+    ];
+    private MailerInterface $mailer;
+    private Environment $engine;
+    private LoggerInterface $logger;
+    private string $mailerDefaultSender;
+
+    public function __construct(
+        MailerInterface $mailer,
+        Environment $engine,
+        LoggerInterface $logger,
+        string $mailerDefaultSender
+    ) {
+        $this->mailer = $mailer;
+        $this->engine = $engine;
+        $this->logger = $logger;
+        $this->mailerDefaultSender = $mailerDefaultSender;
     }
 
-    public function send(): void
+    /**
+     * @throws \Exception
+     */
+    // param array $payload tendra los 2 valores del template [name, url]
+    public function send(string $receiver, string $template, array $payload)
     {
+        // Create email
+        $email = (new Email())
+            ->from($this->mailerDefaultSender)
+            ->to($receiver)
+            ->subject(self::TEMPLATE_SUBJECT_MAP[$template])
+            ->html($this->engine->render($template, $payload));
+
+        try {
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            $this->logger->error(\sprintf('Error sending email: %s', $e->getMessage()));
+        }
     }
 
 }
