6ff3e397579c676fb83c1453e722a7b5c5a179c5
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-13 18:57:46 +0200

    Add: userId, IP in token(payload) with JWTCreated listener

---
+++ api/src/Api/Listener/JWTCreatedListener.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Api\Listener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    // Add UserId && Client IP in token
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        /** @var User $user */
+        $user = $event->getUser();
+        $request = $this->requestStack->getCurrentRequest();
+
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+        $payload['ip'] = $request->getClientIp();
+        unset($payload['roles']);
+
+        $event->setData($payload);
+
+    }
+
+}
