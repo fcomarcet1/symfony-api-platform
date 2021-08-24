776fbd916de9c386d9e1a2e88ad420173b156f79
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-24 12:03:26 +0200

    Config: Change version of FileSystem 2.0 -> 1.5

--- api/src/Service/File/FileService.php
+++ api/src/Service/File/FileService.php
@@ -3,18 +3,49 @@ declare(strict_types=1);
 
 namespace App\Service\File;
 
+use League\Flysystem\FilesystemException;
+use League\Flysystem\FilesystemOperator;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileService
 {
+    private FilesystemOperator $defaultStorage;
+    private LoggerInterface $logger;
+    private string $mediaPath;
 
-    public function __construct()
-    {
+    const VISIBILITY_PUBLIC = 'public';
+    const VISIBILITY_PRIVATE = 'private';
+
+    // The variable name $defaultStorage matters: it needs to be the camelized version
+    // of the name of your storage.
+    public function __construct(
+        FilesystemOperator $defaultStorage,
+        LoggerInterface $logger,
+        string $mediaPath
+    ) {
+        $this->defaultStorage = $defaultStorage;
+        $this->logger         = $logger;
+        $this->mediaPath      = $mediaPath;
     }
 
+    /**
+     * @throws FilesystemException
+     */
     public function uploadFile(UploadedFile $file, string $prefix): string
     {
-        return '';
+        $fileName = \sprintf('%s/%s.%s', $prefix, \sha1(\uniqid()), $file->guessExtension());
+
+
+        // Set Visibility
+        $this->defaultStorage->visibility(self::VISIBILITY_PUBLIC);
+
+        $this->defaultStorage->writeStream(
+            $fileName,
+            \fopen($file->getPathname(), 'r')
+        );
+
+        return $fileName;
     }
 
     public function deleteFile(?string $path): void
