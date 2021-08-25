27dd2ef0557f8f21b67745f1bb459459b0b8a200
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-25 10:23:22 +0200

    Add: Functional test UploadAvatarActionTest case testUploadAvatarWithWrongInputName

--- api/tests/Functional/User/UploadAvatarActionTest.php
+++ api/tests/Functional/User/UploadAvatarActionTest.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 
 class UploadAvatarActionTest extends UserTestBase
 {
+    //Happy path
     public function testUploadAvatar(): void
     {
         // Create file for send to endpoint
@@ -27,6 +28,25 @@ class UploadAvatarActionTest extends UserTestBase
         $response = self::$peter->getResponse();
 
         $this->assertEquals(JsonResponse::HTTP_CREATED, $response->getStatusCode());
+    }
+
+    public function testUploadAvatarWithWrongInputName(): void
+    {
+        $avatar = new UploadedFile(
+            __DIR__ . '/../../../fixtures/avatar.png',
+            'avatar.png'
+        );
+
+        self::$peter->request(
+            'POST',
+            \sprintf('%s/%s/avatar', $this->endpoint, $this->getPeterId()),
+            [],
+            ['non-valid-input' => $avatar]
+        );
+
+        $response = self::$peter->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_BAD_REQUEST, $response->getStatusCode());
     }
 
 }
