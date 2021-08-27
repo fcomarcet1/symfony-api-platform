fd2f949f5787a00776ad5aa785764b57949b6551
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-27 10:46:01 +0200

    Add: GroupVoter

---
+++ api/src/Security/Authorization/Voter/GroupVoter.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Security\Authorization\Voter;
+
+use App\Entity\Group;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class GroupVoter extends Voter
+{
+
+    // Permissions for logged user he can read your files, update, delete (only the user logged)
+    public const GROUP_READ = 'GROUP_READ';
+    public const GROUP_UPDATE = 'GROUP_UPDATE';
+    public const GROUP_DELETE = 'GROUP_DELETE';
+    //public const GROUP_CREATE = 'GROUP_CREATE';
+
+
+    // En este metodo indicamos attrs a soportar, si el atributo esta en el array -> true
+    protected function supports(string $attribute, $subject): bool
+    {
+        return \in_array($attribute, $this->supportedAttributes(), true);
+    }
+
+    /**
+     * El parametro $subject sera un Group o null si deseamos recuperar una coleccion.
+     *
+     * @param Group|null $subject
+     *
+     * El parametro $subject sera el grupo que queremos obtener, y en el TokenInterface tenemos
+     * el token del usuario identificado
+     */
+    protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
+    {
+        if (\in_array($attribute, $this->supportedAttributes(), true)) {
+            return $subject->isOwnerBy($token->getUser());
+        }
+
+        return false;
+    }
+
+    private function supportedAttributes(): array
+    {
+        return [
+            self::GROUP_READ,
+            self::GROUP_UPDATE,
+            self::GROUP_DELETE,
+            //self::GROUP_CREATE,
+        ];
+    }
+}
