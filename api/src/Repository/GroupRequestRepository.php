54edda1c91bf54411934ee316f76376ec533deb4
fcomarcet1 <fcomarcet1@gmail.com>
2021-08-30 13:35:34 +0200

    Add: GroupRequestRepository

---
+++ api/src/Repository/GroupRequestRepository.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\GroupRequest;
+use App\Exception\GroupRequest\GroupRequestNotFoundException;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+
+class GroupRequestRepository extends BaseRepository
+{
+
+    protected static function entityClass(): string
+    {
+        return GroupRequest::class;
+    }
+
+    /**
+     * Find pending user by groupId, userId, token
+     */
+    public function findOnePendingByGroupIdUserIdAndTokenOrFail(
+        string $groupId,
+        string $userId,
+        string $token
+    ) :GroupRequest
+    {
+        $groupRequest = $this->objectRepository->findOneBy([
+            'user' => $userId,
+            'group' => $groupId,
+            'token' => $token,
+            'acceptedAt' => GroupRequest::PENDING
+        ]);
+
+        if ($groupRequest === null){
+            throw GroupRequestNotFoundException::fromGroupIdUserIdAndToken($groupId, $userId, $token);
+        }
+
+        return $groupRequest;
+    }
+
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function save(GroupRequest $groupRequest): void
+    {
+        $this->saveEntity($groupRequest);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function remove(GroupRequest $groupRequest): void
+    {
+        $this->removeEntity($groupRequest);
+    }
+
+    /**
+     * @throws OptimisticLockException
+     * @throws ORMException
+     */
+    public function refresh(GroupRequest $groupRequest): void
+    {
+        $this->refreshEntity($groupRequest);
+    }
+}
